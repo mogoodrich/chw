090f4ae6f29fa8d9014ddfd02f7814e4510c0add
Mark Goodrich <mgoodrich@pih.org>
2012-03-15 14:55:38 -0400

    changed name of constant

--- api/src/main/java/org/openmrs/module/chw/CHWConstants.java
+++ api/src/main/java/org/openmrs/module/chw/CHWConstants.java
@@ -5,6 +5,6 @@ public class CHWConstants {
 
 	public static final String CHW_TYPE_PROVIDER_ATTRIBUTE_TYPE_UUID = "123";
 	
-	public static final String CHW_SUPERVISOR_RELATIONSHIP_TYPE = "567";
+	public static final String CHW_SUPERVISOR_RELATIONSHIP_TYPE_UUID = "567";
 	
 }
